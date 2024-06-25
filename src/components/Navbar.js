672653924e8dfba17428bbba7aca989d46a21427
pierregaly33 <galy.pierre31@gmail.com>
2024-06-25 12:18:50 +0200

    ajout navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Logo from "../assets/img/logo.svg";
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <header className="navbar-main">
+            <img src={Logo} alt="Logo" />
+            <nav className="navbar">
+                <ul>
+                    <NavLink className="accueil" to="/">
+                        <li>Accueil</li>
+                    </NavLink>
+                    <NavLink className="profil" to="/">
+                        <li>Profil</li>
+                    </NavLink>
+                    <NavLink className="reglage" to="/">
+                        <li>Réglage</li>
+                    </NavLink>
+                    <NavLink className="communaute" to="/">
+                        <li>Communauté</li>
+                    </NavLink>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;

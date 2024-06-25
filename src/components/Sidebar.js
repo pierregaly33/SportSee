fcb42cb50b3481071620ac72451e5769a69c6ab7
pierregaly33 <galy.pierre31@gmail.com>
2024-06-25 15:12:12 +0200

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,29 @@
 import React from "react";
+import yoga from "../assets/img/yoga.svg";
+import nage from "../assets/img/nage.svg";
+import velo from "../assets/img/velo.svg";
+import haltere from "../assets/img/haltere.svg";
 
 function Sidebar() {
-    return <div>Sidebar</div>;
+    return (
+        <div className="sidebar-main">
+            <div className="sidebar-button-main">
+                <button className="sidebar-button">
+                    <img src={yoga} alt="" />
+                </button>
+                <button className="sidebar-button">
+                    <img src={nage} alt="" />
+                </button>
+                <button className="sidebar-button">
+                    <img src={velo} alt="" />
+                </button>
+                <button className="sidebar-button">
+                    <img src={haltere} alt="" />
+                </button>
+            </div>
+            <p>Copiryght, SportSee 2020</p>
+        </div>
+    );
 }
 
 export default Sidebar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
-    return <Navbar />;
+    return (
+        <>
+            <Navbar />
+            <Sidebar />
+        </>
+    );
 }
 
 export default Home;
